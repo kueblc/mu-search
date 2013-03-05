59f1ab02e3c875b0ed9bcb67fd48720f2240176d
rebecca <pointeplusplus@gmail.com>
2013-03-04 22:02:35 -0500

    only adding words that are in our keywords, but then also returning the total number of words on the page

--- countWords.py
+++ countWords.py
@@ -32,6 +32,39 @@ def countWords(URL):
 		
 	return wordCount
 	
+	
+def countWordsFast(URL, keyWords):
+
+	##pageContent is a string holding the contents of the webpage
+	pageContent = urllib.urlopen(URL).read()
+	
+	#pageContent = remove_tags.remove_tags(pageContent)
+	
+	
+	##separatedWords is an array of strings used to hold the split contents of the page
+	separateWords = pageContent.split()
+	
+	##get rid of punctuation
+	strippedWords = []
+	for word in separateWords:
+		word = re.sub('[%s]'%re.escape(string.punctuation),'',word)
+		strippedWords.append(word)
+	
+	wordCount = {}
+	
+	count = 0;
+	for word in strippedWords:
+		count += 1;
+		if word in keyWords:
+			if word in wordCount:
+				wordCount[word] += 1
+			else:
+				wordCount[word] = 1
+			
+		
+	return wordCount, count
+
+	
 def wordPerLength(wordCounter, word):
 	#wordCounter = countWords(URL)
 	counter = 0.0
